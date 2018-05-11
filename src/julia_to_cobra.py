4777d384fc82a37bcf280ea84d5af55a439623ff
nlarusstone <nlarusstone@college.harvard.edu>
2018-05-11 10:38:19 +0100

    Initial commit

---
+++ src/julia_to_cobra.py
@@ -0,0 +1,114 @@
+import json
+import os
+
+def convert_rxn(julia_rxn, debug_dict):
+    rxn_name = julia_rxn['reaction_name']
+    debug_entry = debug_dict[rxn_name]
+    metabs = {react["symbol"]: int(-1 * float(react["stoichiometry"])) for react in julia_rxn["list_of_reactants"]}
+    metabs.update({product["symbol"]: int(float(product["stoichiometry"])) for product in julia_rxn["list_of_products"]})
+    cobra_rxn = {
+        'id': rxn_name,
+        'name': rxn_name,
+        'lower_bound': debug_entry['lower_bound'],
+        'upper_bound': debug_entry['upper_bound'],
+        'metabolites': metabs
+    }
+    return cobra_rxn
+
+def convert_metab(julia_metab, atom_dict):
+    sym = julia_metab['species_symbol']
+    cobra_metab = {
+        'compartment': julia_metab['species_compartment'],
+        'id': sym,
+        'name': sym,
+        'formula': atom_dict[sym] if sym in atom_dict else None
+    }
+    return cobra_metab
+
+def parse_atom_file(f_atom):
+    atom_dict = {}
+    with open(f_atom, 'r') as f:
+        for line in f.readlines():
+            l = line.strip().split(',')
+            if len(l) < 7:
+                continue
+            metab_name = l[0]
+            metab_formula = 'C{0}H{1}N{2}O{3}P{4}S{5}'.format(*[l[i] for i in range(1, 7)])
+            #C,H,N,O,P,S
+            atom_dict[metab_name] = metab_formula
+    return atom_dict
+
+def parse_bounds(f_bounds, rxn_dict, idx_map):
+    with open(f_bounds, 'r') as f_bounds:
+        if f_bounds == 'DataDictionary.jl':
+            bounds = False
+            for i, l in enumerate(f_bounds.readlines()):
+                line = l.strip().split() 
+                if '];' in line:
+                    bounds = False
+                if bounds:
+                    lb, ub = float(line[0]), float(line[1])
+                    idx = int(line[6]) - 1
+                    rxn_name = idx_map[i]
+                    rxn_dict[rxn_name]['lower_bound'] = lb 
+                    rxn_dict[rxn_name]['upper_bound'] = ub
+                if 'default_bounds_array' in line:
+                    bounds = True
+        else:
+            for idx, l in enumerate(f_bounds.readlines()):
+                line = l.strip().split('\t') 
+                lb, ub = map(float, line)
+                rxn_name = idx_map[idx]
+                rxn_dict[rxn_name]['lower_bound'] = lb 
+                rxn_dict[rxn_name]['upper_bound'] = ub
+    return rxn_dict
+            
+def parse_debug_file(f_debug, f_bounds):
+    rxn_dict = {}
+    idx_map = {}
+    with open(f_debug, 'r') as f_debug:
+        for line in f_debug.readlines():
+            try:
+                idx, rxn = line.strip().split(' ', 1)
+            except ValueError:
+                break
+            rxn_name, rxn_str = rxn.split('::')
+            # Julia is 1 indexed
+            idx_map[int(idx) - 1] = rxn_name
+            rxn_dict[rxn_name] = {'formula': rxn_str}
+    rxn_dict = parse_bounds(f_bounds, rxn_dict, idx_map)
+    return rxn_dict
+
+def julia_to_cobra_json(folder, fout=None):
+    f_atom = os.path.join(folder, 'Atom.txt')
+    atom_dict = parse_atom_file(f_atom)
+    f_debug = os.path.join(folder, 'Debug.txt')
+    f_bounds = os.path.join(folder, 'Bounds.txt') #DataDictionary.jl#Bounds.txt
+    rxn_dict = parse_debug_file(f_debug, f_bounds)
+    f_model = os.path.join(folder, 'Reactions.json')
+    with open(f_model, 'r') as f:
+        julia_model = json.load(f)
+
+    cobra_model = {'id': 'cell_free_varner'}
+    cobra_metabs, cobra_rxns = [], []
+    cmpts = set()
+    for julia_metab in julia_model["list_of_species"]:
+        cobra_metab = convert_metab(julia_metab, atom_dict)
+        cmpts.add(cobra_metab['compartment'].lower())
+        cobra_metabs.append(cobra_metab)
+    for julia_rxn in julia_model["list_of_reactions"]:
+        cobra_rxn = convert_rxn(julia_rxn, rxn_dict)
+        cobra_rxns.append(cobra_rxn)
+    cobra_model['metabolites'] = cobra_metabs
+    cobra_model['reactions'] = cobra_rxns
+    cobra_model['genes'] = []
+    cobra_cmpts = {}
+    for cmpt in cmpts:
+        cobra_cmpts[cmpt[0]] = cmpt.capitalize()
+    cobra_model['compartments']  = cobra_cmpts
+    if fout:
+        with open(fout, 'w') as outfile:
+            json.dump(cobra_model, outfile)
+    return cobra_model
+
+julia_to_cobra_json('../3rd_Party_Code/Sequence-Specific-FBA-CFPS-Publication-Code/Staging/', '../models/varner.json')
---
+++ src/utils.py
@@ -0,0 +1,161 @@
+import cobra
+import cobra.test
+import numpy as np
+
+def convert_cmpts(model, metab, from_cmpt='c', to_cmpt='e'):
+    if type(metab) is cobra.Metabolite:
+        metab = metab.id.rsplit('_', 1)[0]
+    metab_name = metab + '_{0}'.format(to_cmpt)
+    try:
+        metab_cnvrt = model.metabolites.get_by_id(metab_name)
+    except KeyError:
+        metab_cnvrt = model.metabolites.get_by_id(metab + '_{0}'.format(from_cmpt)).copy()
+        metab_cnvrt.id, metab_cnvrt.compartment = metab_name, to_cmpt
+    return metab_cnvrt
+
+def gen_metab_dict(model, metab_nms, metab_vals=None, cnvt=True, from_cmpt='c', to_cmpt='e'):
+    metabs = []
+    for metab_nm in metab_nms:
+        if cnvt:
+            metab = convert_cmpts(model, metab_nm, from_cmpt, to_cmpt)
+        else:
+            metab = metab_nm
+        metabs.append(metab)
+    if not metab_vals:
+        metab_vals = [0] * len(metabs)
+    metab_conc = dict(zip(metabs, metab_vals))
+    return metab_conc
+
+def add_exchange(model, metab_dict, additive=False):
+    rxn_dict = {}
+    for med_met, conc in metab_dict.items():
+        if conc < 0:
+            ty = 'exchange'
+            ty_name = 'EX'
+	    lb, ub = conc, 0
+        else:
+            ty = 'demand'
+            ty_name = 'DM'
+	    lb, ub = conc, 1000
+        rxn_nm = '{0}_'.format(ty_name) + med_met.id
+        if rxn_nm in model.reactions:
+            rxn = model.reactions.get_by_id(rxn_nm)
+            if additive:
+                rxn.lower_bound,rxn.upper_bound = lb + rxn.lower_bound, ub + rxn.upper_bound
+            #    conc += rxn.upper_bound
+            #    model.reactions.remove(rxn)
+            #    model.add_boundary(metabolite=med_met, type='exchange', lb=conc)
+            else:
+                rxn.lower_bound, rxn.upper_bound = lb, 0
+        else:
+            model.add_boundary(metabolite=med_met, type='medium-diff', reaction_id=rxn_nm, lb=lb, ub=0)
+        rxn_dict[model.reactions.get_by_id(rxn_nm)] = conc
+        #print model.reactions.get_by_id(rxn_nm).upper_bound
+    return rxn_dict
+
+# Based on MetaboTools
+# flux = metConc/(cellConc*cellWeight*t*1000);
+# cellConc: 10 mg/ml
+# cellWeight: 500 * (10 ** -11)
+# t = 24
+# t: Experiment duration
+def conc_to_flux(metab_conc, t=24):
+    # Taken from MetaboTools, units are gDW/cell
+    #cell_weight = 500 * (10 ** -11)
+    # We have 10 mg/ml
+    # need cells / ml
+    cell_conc = 10 * (1/ 1000.0) #* (1 / cell_weight)
+    flux = metab_conc / (cell_conc * t * 1000.0)
+    return flux * 100
+
+def build_medium(mod):
+    model = mod.copy()
+    # in mM
+    aa_conc = -1 * conc_to_flux(2)
+    trna_conc = -1 * conc_to_flux(0.17)
+    metab_conc = map(lambda x: conc_to_flux(x) * -1, [1.2, 0.85, 0.85, 0.85, 0.33, 0.27, 1.50, 1.00, 130, 10, 12, 0.33])
+    # TODO: map from conc to exchange fluxes
+    
+    import Bio.PDB.Polypeptide
+    aas = map(lambda x: x.lower(), Bio.PDB.Polypeptide.aa3)
+    aa_concs = {}
+    for aa in aas:
+        l_aa, d_aa = None, None
+        try:
+            l_aa = convert_cmpts(model, model.metabolites.get_by_id(aa + '__L_c'), from_cmpt='c', to_cmpt='e')
+            d_aa = convert_cmpts(model, model.metabolites.get_by_id(aa + '__D_c'), from_cmpt='c', to_cmpt='e')
+        except KeyError:
+            if aa == 'gly':
+                l_aa = convert_cmpts(model, model.metabolites.get_by_id(aa + '_c'), from_cmpt='c', to_cmpt='e')
+        if d_aa:
+            aa_concs[l_aa] = aa_conc / 2.0
+            aa_concs[d_aa] = aa_conc / 2.0
+        else:
+            aa_concs[l_aa] = aa_conc
+            
+    trna_concs = {convert_cmpts(model, trna_metab, from_cmpt='c', to_cmpt='e') : trna_conc for trna_metab in model.metabolites.query('trna')}
+    
+    metab_nms = ['atp', 'gtp', 'utp', 'ctp', 'nad', 'coa', 'spmd', 'ptrc', 'k', 'nh4', 'mg2', 'pep']
+    metab_concs = gen_metab_dict(model, metab_nms, metab_conc)
+        
+    # Not included: RNAP & Folinic Acid & cell extract
+    base_medium = {}
+    base_medium.update(add_exchange(model, metab_concs))
+    base_medium.update(add_exchange(model, aa_concs))
+    base_medium.update(add_exchange(model, trna_concs))
+    
+    #model.medium = base_medium
+    return model
+
+def add_addl_reactants(model, df):
+    #mod = model.copy()
+    addl_reagent_nms = ['mg2', 'nh4', 'k', 'glc__D', 'pi', 'nad', 'atp', 'coa']
+    objs = []
+    for i, row in df.iterrows():
+	with model as mod:
+	    metab_dict = gen_metab_dict(mod, addl_reagent_nms, map(lambda x: -1 * conc_to_flux(x), row[4:]))
+	    rxn = add_exchange(mod, metab_dict, additive=True)
+	    #mod.add_reactions(reaction_list=sol[0])
+	    obj = mod.slim_optimize()
+            if np.isnan(obj):
+                obj = 0
+	    objs.append(obj)
+	    #print 'Obj: {0}'.format(obj.objective_value)
+    return objs
+
+def gen_fluxes_addl_reactants(model, df):
+    #mod = model.copy()
+    addl_reagent_nms = ['mg2', 'nh4', 'k', 'glc__D', 'pi', 'nad', 'atp', 'coa']
+    objs, fluxes = [], []
+    for i, row in df.iterrows():
+	with model as mod:
+	    metab_dict = gen_metab_dict(mod, addl_reagent_nms, map(lambda x: -1 * conc_to_flux(x), row[4:]))
+	    rxn = add_exchange(mod, metab_dict, additive=True)
+	    #mod.add_reactions(reaction_list=sol[0])
+	    sol = mod.optimize()
+	    objs.append(sol.objective_value)
+            fluxes.append(sol.fluxes)
+    return objs, fluxes
+
+def gen_fluxes(model):
+    obj = 0
+    with model as mod:
+        sol = mod.optimize()
+        obj = sol.objective_value
+        fluxes = sol.fluxes
+    return obj, fluxes
+
+def change_obj(model, metab):
+    if (not 'DM_{0}'.format(metab.id) in model.reactions):
+	model.add_boundary(metabolite=metab, type='demand')
+    model.objective = model.reactions.get_by_id('DM_{0}'.format(metab.id))
+
+def different_mediums(model1, model2):
+    joint_keys = set(model1.medium.keys()).union(set(model2.medium.keys()))
+    for key in joint_keys:
+        if key in model2.medium and key in model1.medium:
+            print 'both: ', key, model1.medium[key], model2.medium[key]
+        elif key in model1.medium:
+            print 'Model 1: ', key, model1.medium[key]
+        elif key in model2.medium:
+            print 'Model 2: ', key, model2.medium[key]
