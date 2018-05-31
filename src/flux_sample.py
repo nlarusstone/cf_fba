9c102876c1bba2716a39c73798a47dfce1a39d60
nlarusstone <nlarusstone@college.harvard.edu>
2018-05-31 13:54:58 +0100

    Updated flux sample code

--- src/flux_sample.py
+++ src/flux_sample.py
@@ -11,7 +11,10 @@ import argparse
 parser = argparse.ArgumentParser(description='Sample fluxes from different CF models')
 parser.add_argument('-s', '--samps', metavar='s', type=int, help='Number of samples', default=200)
 parser.add_argument('-d', '--dataset', type=str, default='nls')
-
+parser.add_argument('-f', '--froot', type=str, default='hand')
+parser.add_argument('-r', '--rxn', type=int, default=5, help='Amount in uL for a reaction')
+parser.add_argument('-a', '--addl', type=int, default=1, help='Additional amount of reactants added')
+parser.add_argument('-b', '--batch_size', type=int, default=50, help='Total amount of liquid in uL in a batch')
 
 def get_aa_metab(model, aa, cmpt='c'):
     return model.metabolites.query('{0}__._{1}'.format(aa, cmpt))
@@ -39,38 +42,61 @@ def change_conc(model, cfps_conc):
 def conc_dilution(start_conc, vol_add, tot_vol):
     return start_conc * (vol_add / tot_vol)
 
-def get_exp_data():
-    df = pd.read_csv('../data/17_5_18_T7_mRFP_NLS.CSV', skiprows=6)
+def get_exp_data(froot):
+    # '../data/17_5_18_T7_mRFP_NLS.CSV'
+    df = pd.read_csv('../data/{0}_data.CSV'.format(froot), skiprows=6)
     print df.shape
     gain_diff = df.shape[0] / 5
     times = df["Unnamed: 1"]
     df.drop('Unnamed: 1', inplace=True, axis=1)
     # Bad data
-    df.drop(['E09', 'F04', 'F05'], inplace=True, axis=1)
-    df.head()
+    if froot == 'hand':
+        df.drop('E09', inplace=True, axis=1)
+    # Remove negative control
+    df.drop(df.columns[-2:], inplace=True, axis=1)
+    
     gain2 = gain_diff
     outs = df[gain2:gain2+gain_diff - 1].mean(axis=0)
-    conds = pd.read_csv('../data/17_5_18_exp_conditions.csv')
-    conds.drop(conds.shape[0] - 1, inplace=True)
-    conds_full = conds.reindex(np.repeat(conds.index.values, 2)).reset_index()
-    conds_full = conds_full.drop(32).reset_index(drop=True)
+    # '../data/17_5_18_exp_conditions.csv'
+    conds = pd.read_csv('../data/{0}_exp_conditions.csv'.format(froot))
+    if froot == 'hand':
+        conds.drop(conds.shape[0] - 1, inplace=True)
+        conds_full = conds.reindex(np.repeat(conds.index.values, 2)).reset_index()
+        conds_full = conds_full.drop(32).reset_index(drop=True)
+    else:
+        conds_full = conds.reindex(np.repeat(conds.index.values, 2)).reset_index()
     conds_full['OUT'] = outs.reset_index(drop=True)
     conds_avg = conds_full.groupby('index').mean()
     conds_norm = conds_avg / conds_avg.max()
-    conds_norm.to_csv('../data/17_5_18_EXPERIMENT.csv')
+    conds_norm.to_csv('../data/{0}_EXPERIMENT.csv'.format(froot))
     return conds_norm
+
+def update_vals(cfps_conc_tmp, row, n_batches):
+    for col in row.index[:-1]:
+        if col == 'nts':
+            for nt in ['atp', 'gtp', 'ctp', 'utp']:
+                cfps_conc_tmp.loc[nt]['amt'] += row['nts'] / 4
+        elif col == 'mdx':
+            for cmpnd, vals in nrg_mix.iterrows():
+                cfps_conc_tmp.loc[cmpnd] = [cfps_conc_tmp.loc[cmpnd]['start_conc'], (5.0 + row['mdx'] / n_batches)]
+        elif col == 'aas':
+            for aa in pdb.Polypeptide.aa3:
+                cfps_conc_tmp.loc[aa] = [cfps_conc_tmp.loc[aa]['start_conc'], (10.0 + row['aas'] / n_batches)]
+        else:
+            cfps_conc_tmp.loc[col]['amt'] += row[col]
+    return cfps_conc_tmp
 
 if __name__ == '__main__':
     args = parser.parse_args()
     flux_sz = args.samps
-    rxn_amt = 5
-    addl_amt = 1
-    batch_size = 50
+    rxn_amt = args.rxn
+    addl_amt = args.addl
+    batch_size = args.batch_size
     n_batches = batch_size / rxn_amt
     models = ['{0}_ecoli_cf_base{1}.sbml'.format(args.dataset, txtl) for txtl in ['_txtl', '']]
 
     print 'Read in data'
-    df = get_exp_data() #pd.read_csv('../data/Karim_MetEng_2018_Figure2_Data.csv')
+    df = get_exp_data(args.froot) #pd.read_csv('../data/Karim_MetEng_2018_Figure2_Data.csv')
     #df.drop(columns=['Area_1', 'Area_2', 'Conc_1', 'Conc_2'], inplace=True)
 
     cfps_conc = pd.read_csv('../data/{0}_concs'.format(args.dataset), index_col='compound')
@@ -85,15 +111,10 @@ if __name__ == '__main__':
         for idx, row in df.iterrows():
             print idx
             cfps_conc_tmp = cfps_conc.copy()
-            cfps_conc_tmp.loc['pi']['amt'] += row['pi']
-            cfps_conc_tmp.loc['k']['amt'] += row['k']
-            for nt in ['atp', 'gtp', 'ctp', 'utp']:
-                cfps_conc_tmp.loc[nt]['amt'] += row['nts'] / 4
-            for cmpnd, vals in nrg_mix.iterrows():
-                cfps_conc_tmp.loc[cmpnd] = [cfps_conc.loc[cmpnd]['start_conc'], (5.0 + row['mdx'] / n_batches)]
+            cfps_conc_tmp = update_vals(cfps_conc_tmp, row, n_batches)
             rxn_amt += addl_amt
             exp_concs = conc_dilution(cfps_conc_tmp['start_conc'], cfps_conc_tmp['amt'], rxn_amt)
             model_i = change_conc(model, exp_concs)
             print model_i.medium
             samples_i = sample(model_i, flux_sz, processes=multiprocessing.cpu_count() - 1)
-            samples_i.to_csv('../data/f{2}/{0}_fluxes_{1}'.format(model_f, idx, flux_sz))
+            samples_i.to_csv('../data/f{2}/{3}_{0}_fluxes_{1}'.format(model_f, idx, flux_sz, args.froot))
