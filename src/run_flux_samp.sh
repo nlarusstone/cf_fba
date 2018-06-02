7cda22cfca20a04c390d77c9edbb9be89f1ef285
Nicholas Larus-Stone <nl363@cl.cam.ac.uk>
2018-06-02 07:25:25 -0400

    Script to run flux sampler

---
+++ src/run_flux_samp.sh
@@ -0,0 +1,10 @@
+samps=(10 200 2000)
+for samp in "${samps[@]}"; do
+    python flux_sample.py -s ${samp} -d 'karim' -f 'karim' -r 25 -a 0 -b 25
+done
+#for samp in "${samps[@]}"; do
+#    python flux_sample.py -s ${samp} -f 'echo' -r 2 -a 0.5
+#done
+#for samp in "${samps[@]}"; do
+#    python flux_sample.py -s ${samp} -f 'hand' -r 5 -a 1
+#done
